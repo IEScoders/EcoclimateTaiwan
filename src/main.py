9c4d0e093b7246e7d09f366d86ac6a994d8a21f1
Chase J. Shyu <iamhemry@gmail.com>
2018-12-07 20:49:42 +0800

    merge ei and coord

--- src/main.py
+++ src/main.py
@@ -12,10 +12,30 @@ def main():
     path_to_rep = '../'
     species = 'solenopsis_invicta'
 
-    #            str         str      
     name, ei = get_ei.get_ei(path_to_rep,species,date)
 
-    print (name, ei)
+    ei_dict = {}
+    for n, e in zip(name, ei):
+      ei_dict[n] = e
+
+    station_file = 'CWB_Stations_171226.csv'
+    df=pd.read_csv(path_to_rep + station_file,delimiter=',')
+
+    station_id = df['id']
+    station_lon = df['long']
+    station_lat = df['lat']
+
+    ei, lon, lat = [], [], []
+    for i, j ,k in zip(station_id,station_lon,station_lat):
+      try:
+        ei.append(ei_dict[i])
+        lon.append(j)
+        lat.append(k)
+      except:
+        pass
+
+    for i, e in enumerate(ei):
+      print (lon[i], lat[i], e)
 
 
 if __name__ == '__main__':
